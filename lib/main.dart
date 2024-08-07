5776e8fde0927a9bc0186a1f2c58846adc982946
kibakuss <duxabank@gmail.com>
2024-08-07 10:25:42 +0500

    search trouble base example

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,161 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
-void main() {
+import 'package:flutter/material.dart';
+import 'package:yandex_maps_mapkit/init.dart' as init;
+import 'package:yandex_maps_mapkit/mapkit.dart';
+import 'package:yandex_maps_mapkit/search.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await init.initMapkit(apiKey: '8b4ef9c3-3ca1-4ebf-92c8-f828255d02f8');
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const AddAdress(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class AddAdress extends StatefulWidget {
+  const AddAdress({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<AddAdress> createState() => _AddAdressState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _AddAdressState extends State<AddAdress> {
+  final TextEditingController _searchController = TextEditingController();
+  List<String> _suggestions = [];
+  late SearchManager searchManager;
+  SearchSession? searchSession;
+  late SearchSessionSearchListener listener;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    log('Инициализация состояния');
+    searchManager =
+        SearchFactory.instance.createSearchManager(SearchManagerType.Combined);
+    _searchController.addListener(_onSearchChanged);
+
+    listener = SearchSessionSearchListener(
+      onSearchResponse: _handleSearchResponse,
+      onSearchError: _handleSearchError,
+    );
+    log('Инициализация завершена');
+  }
+
+  void _handleSearchResponse(SearchResponse response) {
+    log('Получен ответ поиска');
+    final List<String> newSuggestions = response.collection.children
+        .map((item) => item.toString())
+        .where((name) => name != null)
+        .toList();
+    log('Новые предложения: $newSuggestions');
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _suggestions = newSuggestions;
     });
+  }
+
+  void _handleSearchError(error) {
+    print('Произошла ошибка при поиске: $error');
+  }
+
+  void _onSearchChanged() {
+    log('Текст поиска изменен: ${_searchController.text}');
+    if (_searchController.text.isEmpty) {
+      setState(() {
+        _suggestions = [];
+      });
+      return;
+    }
+    _performSearch(_searchController.text);
+  }
+
+  final boundingBox = const BoundingBox(
+      Point(latitude: 41.185, longitude: 19.638),
+      Point(latitude: 81.858, longitude: 180.0));
+  void _performSearch(String query) {
+    log('Выполнение поиска для запроса: $query');
+
+    try {
+      if (searchManager == null) {
+        log('SearchManager не инициализирован');
+        return;
+      }
+
+      final geometry = const Geometry.fromCircle(
+          Circle(Point(latitude: 41.185, longitude: 19.638), radius: 100));
+
+      final searchOptions = SearchOptions(searchTypes: SearchType.None);
+      log('дошли');
+      // searchSession?.cancel();
+      try {
+        searchSession = searchManager.submit(
+          geometry,
+          searchOptions,
+          listener,
+          text: query,
+        );
+      } catch (e) {
+        log('Произошла ошибка при поиске: $e');
+      }
+      if (searchSession != null) {
+        log('Поисковая сессия создана успешно');
+      } else {
+        log('Не удалось создать поисковую сессию');
+      }
+    } catch (e, stackTrace) {
+      log('Ошибка при выполнении поиска: $e');
+      log('Stack trace: $stackTrace');
+    }
+  }
+
+  @override
+  void dispose() {
+    _searchController.removeListener(_onSearchChanged);
+    _searchController.dispose();
+    searchSession?.cancel();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        title: TextField(
+          onChanged: (value) {
+            _performSearch(value);
+          },
+          controller: _searchController,
+          autofocus: true,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Container(
+          color: Colors.white,
+          child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: _suggestions.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(_suggestions[index]),
+                onTap: () {
+                  print('Выбран адрес: ${_suggestions[index]}');
+                },
+              );
+            },
+          )),
     );
   }
 }
